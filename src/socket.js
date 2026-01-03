d95071a6b7beab225322024f03d042fe087ea300
Ashish <ashish@Ashishs-MacBook-Pro.local>
2026-01-03 06:24:12 +0530

    socket url

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5500", {
+const socket = io("https://onway-backend-6kfr.onrender.com", {
   autoConnect: false,
   transports: ["websocket"], 
 });
