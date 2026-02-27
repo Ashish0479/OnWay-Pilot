5ba402af1a48a143b9b5e432d42950589b9ab8f0
Ashish <ashish@Ashishs-MacBook-Pro.local>
2026-02-27 15:49:44 +0530

    sw

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@ self.addEventListener("push", function (event) {
 
   self.registration.showNotification(data.title, {
     body: data.body,
-    icon: "../src/assets/icon.png",
-    badge: "../src/assets/react.svg",
+    icon: "/icon.png",
+    badge: "/icon.png",
   });
 });
